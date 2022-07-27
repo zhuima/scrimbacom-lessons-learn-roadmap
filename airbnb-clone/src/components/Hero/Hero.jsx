6deb6c000b43b83502fe1ea44a67567d6895c607
zhuima <zhuima314@gmail.com>
2022-07-28 00:13:28 +0800

    feature: change display style

--- airbnb-clone/src/components/Hero/Hero.jsx
+++ airbnb-clone/src/components/Hero/Hero.jsx
@@ -1,11 +1,13 @@
 import React from "react";
+import "./index.css";
+
 
 const Hero = () => {
     return (
-        <section>
-            <img src="../../../Group77.png" alt="" />
-            <h1>Online Experiences</h1>
-            <p>Join unique interactive activities led by 
+        <section className="hero">
+            <img src="../../../Group77.png" alt="" className="hero--photo" />
+            <h1 className="hero--header">Online Experiences</h1>
+            <p className="hero--text">Join unique interactive activities led by 
             one-of-a-kind hosts—all without leaving home.</p>
         </section>
     )
